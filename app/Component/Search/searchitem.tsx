59adf53a7e11f1fcb88e878f3365348ccc7bdb6c
savdev <savde388@gmail.com>
2025-04-29 18:14:14 -0700

    changes

--- app/Component/Search/searchitem.tsx
+++ app/Component/Search/searchitem.tsx
@@ -4,7 +4,6 @@ import React, { useEffect, useState, useRef } from 'react'
 import { FiSearch } from "react-icons/fi";
 import Link from "next/link";
 import Image from 'next/image';
-import { motion, AnimatePresence } from "framer-motion";
 
 interface SearchProps {
     isOpen: boolean;
@@ -26,13 +25,12 @@ const Page: React.FC<SearchProps> = ({ isOpen, isClose }) => {
     useEffect(() => {
         const handleClick = (event: MouseEvent) => {
             const target = event.target as HTMLElement;
-            // Prevent closing when clicking on search input or cancel button
             if (target.closest("input") || target.closest("#cancel-button")) {
                 return;
             }
             setsearchquery('');
             setresult([]);
-            isClose(); // Close the search container
+            isClose();
         };
 
         if (isOpen) {
@@ -51,11 +49,11 @@ const Page: React.FC<SearchProps> = ({ isOpen, isClose }) => {
         }
 
         const fetchresult = async () => {
-            setloading(true);  // Set loading true before fetching
+            setloading(true);
             const dataapi = await fetch(`https://dummyjson.com/products/search?q=${encodeURIComponent(searchquery)}`);
             const res = await dataapi.json();
             setresult(res.products);
-            setloading(false);  // Set loading false after fetching
+            setloading(false);
         };
 
         fetchresult();
@@ -64,122 +62,91 @@ const Page: React.FC<SearchProps> = ({ isOpen, isClose }) => {
     return (
         <div>
             {isOpen && (
-                <AnimatePresence>
-                    <motion.div
-                        initial={{ opacity: 0, y: -20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -20 }}
-                        transition={{ duration: 0.5, ease: "easeInOut" }}
-                        className="fixed left-0 top-0 w-full z-10 "
+                <div className="fixed left-0 top-0 w-full z-10">
+                    <div
+                        ref={searchRef}
+                        className="w-full h-[50vh] py-1 overflow-y-scroll px-10 bg-white"
                     >
-                        <motion.div
-                            initial={{ height: 0 }}
-                            animate={{ height: "50vh" }}
-                            exit={{ height: 0 }}
-                            transition={{ duration: 0.6, ease: "easeInOut" }}
-                            ref={searchRef}
-                            className="w-full py-1 overflow-y-scroll px-10 bg-white"
-                        >
-                            <div className="flex items-center gap-7 justify-between">
-                                <div className='hidden md:flex'>
-                                    <svg
-                                        aria-hidden="true"
-                                        viewBox="0 0 24 24"
-                                        role="img"
-                                        width="74px"
-                                        height="74px"
-                                        fill="none"
-                                    >
-                                        <path
-                                            fill="currentColor"
-                                            fillRule="evenodd"
-                                            d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z"
-                                            clipRule="evenodd"
-                                        ></path>
-                                    </svg>
-                                </div>
-                                <div className="flex gap-4 bg-[#e5e5e5] group transition-all ease-linear hover:bg-slate-300 h-max w-[680px] rounded-[30px] items-center">
-                                    <div className="px-[7px] rounded-full py-[7px] bg-[#e5e5e5]">
-                                        <FiSearch size={20} />
-                                    </div>
-                                    <input
-                                        value={searchquery}
-                                        onChange={(e) => setsearchquery(e.target.value)}
-                                        className=" w-[50%]    md:w-[600px] group hover:bg-slate-300 outline-none flex-grow rounded-r-[30px] py-[6px] bg-[#e5e5e5]"
-                                        type="text"
-                                        placeholder="search"
-                                    />
-                                </div>
-                                <h3
-                                    id="cancel-button"
-                                    onClick={isClose}
-                                    className="font-bold cursor-pointer text-[17px]"
+                        <div className="flex items-center gap-7 justify-between">
+                            <div className='hidden md:flex'>
+                                <svg
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    role="img"
+                                    width="74px"
+                                    height="74px"
+                                    fill="none"
                                 >
-                                    Cancel
-                                </h3>
+                                    <path
+                                        fill="currentColor"
+                                        fillRule="evenodd"
+                                        d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </svg>
                             </div>
-
-                            {loading && <li>Loading...</li>}
-
-                            <motion.ul
-                                initial={{ opacity: 0, scale: 0.9 }}
-                                animate={{ opacity: 1, scale: 1 }}
-                                transition={{ duration: 0.4, ease: "easeInOut" }}
-                                className="mt-4"
+                            <div className="flex gap-4 bg-[#e5e5e5] group transition-all ease-linear hover:bg-slate-300 h-max w-[680px] rounded-[30px] items-center">
+                                <div className="px-[7px] rounded-full py-[7px] bg-[#e5e5e5]">
+                                    <FiSearch size={20} />
+                                </div>
+                                <input
+                                    value={searchquery}
+                                    onChange={(e) => setsearchquery(e.target.value)}
+                                    className=" w-[50%] md:w-[600px] group hover:bg-slate-300 outline-none flex-grow rounded-r-[30px] py-[6px] bg-[#e5e5e5]"
+                                    type="text"
+                                    placeholder="search"
+                                />
+                            </div>
+                            <h3
+                                id="cancel-button"
+                                onClick={isClose}
+                                className="font-bold cursor-pointer text-[17px]"
                             >
-                                {result.map((item) => (
-              
+                                Cancel
+                            </h3>
+                        </div>
 
-                                    <Link
-                                        key={item.id}
-                                        href={`/productspage/${item.id}`} // Navigate to the product page
-                                        className="p-2 flex items-center justify-between border-b cursor-pointer hover:bg-gray-100"
-                                        onClick={() => isClose()}
-                                    >
-                                        {item.title}
-                                           <Image
-                                                             src={item.images[0] || "/globe.svg"}
-                                                             alt={item.title || "Product Image"}
-                                                             width={300}
-                                                             height={200}
-                                                             className="object-contain w-[60%] h-[150px] md:h-[200px] mx-auto"
-                                                             loading="lazy"
-                                                           />
-                                    </Link>
-                                ))}
-                            </motion.ul>
+                        {loading && <li>Loading...</li>}
 
-                            <motion.div
-                                initial={{ opacity: 0, x: -20 }}
-                                animate={{ opacity: 1, x: 0 }}
-                                transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
-                                className="flex flex-col text-nowrap  w-full md:w-[50%] mt-4 mx-auto"
-                            >
-                                <span className="grid my-2 font-bold">Popular search</span>
-                                <motion.div
-                                    initial={{ opacity: 0, x: 20 }}
-                                    animate={{ opacity: 1, x: 0 }}
-                                    transition={{ duration: 0.5, ease: "easeInOut", delay: 0.3 }}
-                                    className="transform grid  gap-2 text-[14px] grid-cols-4 md:grid-cols-6"
+                        <ul className="mt-4">
+                            {result.map((item) => (
+                                <Link
+                                    key={item.id}
+                                    href={`/productspage/${item.id}`}
+                                    className="p-2 flex items-center justify-between border-b cursor-pointer hover:bg-gray-100"
+                                    onClick={() => isClose()}
                                 >
-                                    {["home", "air max", "jordan", "Drunks", "Drunks", "jordan", "Drunks", "Drunks"].map(
-                                        (item, index) => (
-                                            <span
-                                                key={index}
-                                                className="px-4 w-max text-center py-2 rounded  text-black bg-[#e5e5e5]"
-                                            >
-                                                {item}
-                                            </span>
-                                        )
-                                    )}
-                                </motion.div>
-                            </motion.div>
-                        </motion.div>
-                        <motion.div
-                            className="h-[50vh] w-full opacity-10 bg-[#000000e0]"
-                        ></motion.div>
-                    </motion.div>
-                </AnimatePresence>
+                                    {item.title}
+                                    <Image
+                                        src={item.images[0] || "/globe.svg"}
+                                        alt={item.title || "Product Image"}
+                                        width={300}
+                                        height={200}
+                                        className="object-contain w-[60%] h-[150px] md:h-[200px] mx-auto"
+                                        loading="lazy"
+                                    />
+                                </Link>
+                            ))}
+                        </ul>
+
+                        <div className="flex flex-col text-nowrap w-full md:w-[50%] mt-4 mx-auto">
+                            <span className="grid my-2 font-bold">Popular search</span>
+                            <div className="transform grid gap-2 text-[14px] grid-cols-4 md:grid-cols-6">
+                                {["home", "air max", "jordan", "Drunks", "Drunks", "jordan", "Drunks", "Drunks"].map(
+                                    (item, index) => (
+                                        <span
+                                            key={index}
+                                            className="px-4 w-max text-center py-2 rounded text-black bg-[#e5e5e5]"
+                                        >
+                                            {item}
+                                        </span>
+                                    )
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[50vh] w-full opacity-10 bg-[#000000e0]"></div>
+                </div>
             )}
         </div>
     );
