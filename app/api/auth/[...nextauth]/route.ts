49ea60cfaadfb3f82c90de7fb568707efa47cc12
savdev <savde388@gmail.com>
2025-11-07 07:13:39 -0800

    ii

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-// app/api/auth/[...nextauth]/route.ts
-import { handlers } from '@/lib/auth'
+// // app/api/auth/[...nextauth]/route.ts
+// import { handlers } from '@/lib/auth'
 
-export const { GET, POST } = handlers
+// export const { GET, POST } = handlers
